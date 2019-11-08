300a6faa389e33f11f9e9fc8dd6b52169a4ec109
anthony.jhw <hyunwook0506@khu.ac.kr>
2019-11-08 11:51:29 +0900

    (HW)models.py_RetireCalculate를 추가하였습니다. , admin.py

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import RetireCalculate
 # Register your models here.
+
+admin.site.register(RetireCalculate)
--- hello/models.py
+++ hello/models.py
@@ -7,39 +7,48 @@ class RetireCalculate(models.Model):
     # ThreeMonthCost = 평균 3개월 임금금액
     # YearPerformance = 연간 상여금 총액
     # YearAllowance = 연차수당
-    ThreeMonthCost = models.CharField(max_length=100)
-    YearPerformance = models.CharField(max_length=100)
-    YearAllowance = models.CharField(max_length=100)
-    WorkDays = models.CharField(max_length=100)
-    ShowPensionDB = ((int(ThreeMonthCost) + int(YearPerformance)*3/12 + YearAllowance*3/12)/90)*30*int(WorkDays/365)   
+    ThreeMonthCost = models.IntegerField()
+    YearPerformance = models.IntegerField()
+    YearAllowance = models.IntegerField()
+    WorkDays = models.IntegerField()
+    RetireExpected = models.CharField(max_length=255, default='example')
+
+    #계산 및 데이터 베이스 저장
+    def ExpectedRevenue(self):
+        self.RetireExpected = ''
+        Revenue = (self.ThreeMonthCost+self.YearPerformance*3/12 + self.YearAllowance*3/12)
+        Revenue2 = Revenue/90*30*(self.Workdays/365)
+        self.RetireExpected = str(Revenue2)
+        
+        self.save()
 
 
-class PrivacyPension(models.Model):
-    PensionCategory =(
-        ('P1','연금보험'),
-        ('P2','변액연금보험'),
-        ('P3','연금저축보험'),
-    )
+
+# class PrivacyPension(models.Model):
+#     PensionCategory =(
+#         ('P1','연금보험'),
+#         ('P2','변액연금보험'),
+#         ('P3','연금저축보험'),
+#     )
     
-    PensionRateReturn = models.CharField(max_length=100)
-    MonthCompound = models.CharField(max_length = 100)
+#     PensionRateReturn = models.CharField(max_length=100)
+#     MonthCompound = models.CharField(max_length = 100)
 
 
-class ShowPensionDC(models.Model):
-    investment = (
-        ('i1','10'),
-        ('i2','15'),
-        ('i3','20'),
-    )
-    YearDC = (
-        ('y1','10'),
-        ('y2','15'),
-        ('y3','20'),
-    )
-    RateReturnDC = models.CharField(max_length = 100)
-    TotalAllowanceDC = int(investment)*int(RateReturnDC)
+# class ShowPensionDC(models.Model):
+#     investment = (
+#         ('i1','10'),
+#         ('i2','15'),
+#         ('i3','20'),
+#     )
+#     YearDC = (
+#         ('y1','10'),
+#         ('y2','15'),
+#         ('y3','20'),
+#     )
+#     RateReturnDC = models.CharField(max_length = 100)
     
-    #get_필드명_display()
-class ChoicePension(models.Model):
-    WantYear = models.CharField(max_length = 100)
-    ChoicePensionAnswer()
+#     #get_필드명_display()
+# class ChoicePension(models.Model):
+#     WantYear = models.CharField(max_length = 100)
+#     AllPensionResult = 
